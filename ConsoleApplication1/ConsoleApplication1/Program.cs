6fc5361e9b6a788fc0410eecc1f435aa1d2b34d9
GT1876 <garrysontime@gmail.com>
2014-04-30 10:12:05 -0400

    problem

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            string o;
+            Console.Write("Enter a number:");
+            o = Console.ReadLine();
+            n = Convert.ToInt32(o);
+            Console.Write("W");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("o");
+            }
+            {
+                Console.Write("W!");
+            }
+
+
+
+        }
+       
+    }
+}
