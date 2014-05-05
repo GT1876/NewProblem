f781c10137f052a630c8742ec45bdd01298e347a
GT1876 <garrysontime@gmail.com>
2014-05-05 10:08:41 -0400

    fhwe

---
+++ 2/2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                bool a = true;
+                Console.Write("Enter a word:");
+                string word = Console.ReadLine().ToLower();
+                if (word == "quit")
+                    break;
+
+                //Print out if word is pallindrome
+
+                for (int i = 1; i < word.Length; i++)
+                {
+                    if (word[i] != word[word.Length - 1 - i])
+                    {
+                        a = false;
+                    }
+                }
+
+                if (a)
+                {
+                    Console.WriteLine("Yasssss");
+                }
+                else
+                {
+                    Console.WriteLine("Nooooooo");
+                }
+
+            }
+        }
+    }
+}
